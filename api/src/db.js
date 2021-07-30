9ec4905fe429ca60a1c69f32d4468cae64642f9c
Any <anyeldayana@gmail.com>
2021-07-30 14:00:58 -0500

    All

--- api/src/db.js
+++ api/src/db.js
@@ -30,10 +30,14 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Character } = sequelize.models;
+const { Character, Occupation } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+
+Character.belongsToMany(Occupation, {through: 'character_occuption'});
+Occupation.belongsToMany(Character, {through: 'character_occuption'});
+
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Landing from './components/Landing/index.jsx';
+import Home from './components/Home/index.jsx';
+import AddCharacter from './components/AddCharacter/index.js';
+import Details from './components/Details/index.jsx';
 
 function App() {
   return (
     <div className="App">
-      <h1>Breaking Bad App</h1>
+      <BrowserRouter>
+        <Route exact path='/' component={Landing}></Route>
+        <Route exact path='/characters' component={Home}></Route>
+        <Route exact path='/character' component={AddCharacter}></Route>
+        <Route exact path='/details/:id' component={Details}></Route>
+      </BrowserRouter>
     </div>
   );
 }
